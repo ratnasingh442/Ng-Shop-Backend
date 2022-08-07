fe6731d0261ca756a00b2ff186c009f494b54df9
ratna singh <ratnasingh442@gmail.com>
2022-08-07 17:49:51 +0530

    Adding authentication,routes,models,images

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const express = require("express");
 const app = express();
-require('dotenv/config')
-app.use(express.json())
-app.use(morgan('tiny'))
-const api = process.env.API_URL
-app.post(`${api}/products`,(req,res)=>{
-    res.send(req.body);
-    const product = {
-        id: 1,
-        name: "hair dresser",
-        image: "some_url"
-    }
-    console.log(req);
-})
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
+
+app.use(cors());
+app.options("*", cors());
+
+//middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authJwt());
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+app.use(errorHandler);
+
+//Routes
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
 const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-  );
-  mongoose
-    .connect(DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology:true,
-      dbName: 'db1'
-    })
-    .then(() => {
-      console.log('DB connection successful');
-      // console.log(con.connection);
-    }).catch((err)=>{
-        console.log(err);
-    });
-  
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-})
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    dbName: 'db1'
+  })
+  .then(() => {
+    console.log('DB connection successful');
+    // console.log(con.connection);
+  }).catch((err)=>{
+      console.log(err);
+  });
+
+//Server
+app.listen(3000, () => {
+  console.log("server is running http://localhost:3000");
+});
